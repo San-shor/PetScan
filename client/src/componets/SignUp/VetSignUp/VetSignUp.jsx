50d7c8e70668dc731b31288a00ec03c79f320c5f
ASIF BIN ABEDIN <asif@ASIFs-MacBook-Pro.local>
2023-04-30 10:30:32 +0600

    working on vet signup

--- client/src/componets/SignUp/VetSignUp/VetSignUp.jsx
+++ client/src/componets/SignUp/VetSignUp/VetSignUp.jsx
@@ -8,8 +8,9 @@ import apiVet from "../../../ApiServices/ApiVetServices";
 import userWithPet from "../../../assets/signup/user-with-pet.svg";
 import "./VetSignUp.css";
 import AnnonymousBar from "../../NavBar/AnnonymousBar/AnnonymousBar";
+import Alert from '@mui/material/Alert';
 
-const PASS_MIN_LENGTH = 8;
+// const PASS_MIN_LENGTH = 8; (Legacy: No use )
 
 const initialState = {
   firstName: "",
@@ -23,6 +24,11 @@ const VetSignUp = () => {
   let navigate = useNavigate();
   const [state, setState] = useState(initialState);
   const [showPassword, setShowPassword] = useState(false);
+
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,7 +45,13 @@ const VetSignUp = () => {
   const createUser = async (e) => {
     e.preventDefault();
     // extract the user data from the state
-    const { firstName, lastName, email, password, confirmPassword } = state;
+    const { firstName, lastName, email, password } = state;
+
+    if (!firstName.trim()) return console.log('First Name is required');
+    if (!lastName.trim()) return console.log('Last Name is required');
+    if (!email.trim()) return console.log('Email is required');
+    if (!password.trim() || password.length<5) return console.log('Password Should be at least 5 characters long ');
+
     const newVet = {
       firstName,
       lastName,
@@ -48,9 +60,9 @@ const VetSignUp = () => {
     };
      
     console.log(newVet);
+    
     // send the user data to the server
     try {
-      console.log('try er vhitor ashce');
       const response = await apiVet.signup(newVet);
       // save the token in the local storage
       const { accessToken, fullName, email, user } = response;
@@ -65,32 +77,34 @@ const VetSignUp = () => {
     }
   };
 
-  const validateForm = () => {
-    return (
-      state.firstName.length > 0 &&
-      state.lastName.length > 0 &&
-      state.email.length > 0 &&
-      state.password.length > 0 &&
-      state.confirmPassword.length > 0
-    );
-  };
+  // const validateForm = () => {
+  //   return (
+  //     state.firstName.length > 0 &&
+  //     state.lastName.length > 0 &&
+  //     state.email.length > 0 &&
+  //     state.password.length > 0 &&
+  //     state.confirmPassword.length > 0
+  //   );
+  // }; (Legacy : never been used)
 
   return (
     <>
+    
       <section className="vet-container">
         <AnnonymousBar />
         <div className="signup-form">
           <div className="vet-header">
             <img className="logo" src={userWithPet} alt="logo" />
             <h1>Create a Vet account</h1>
-            <p>
+            {/* <p>
               <b>Or</b>
             </p>
             <span>
               <Link to={"/signin"}>
-                <strong>Sign in to an existent account</strong>
+                <strong>Sign in to an existent account</strong> 
               </Link>
             </span>
+            // (Legacy : Unneccessary)  */}
           </div>
           {/* <div className="auth-account">
             <span>Create account with</span>
@@ -119,7 +133,12 @@ const VetSignUp = () => {
               }}
               noValidate
               autoComplete="off"
-            >
+            ><Stack sx={{ width: '100%' }} spacing={2}>
+            <Alert severity="error">First name is required</Alert>
+            <Alert severity="error">Last Name is required</Alert>
+            <Alert severity="error">Email is required</Alert>
+            <Alert severity="error">Password Should be at least 5 characters long</Alert>
+          </Stack>
               <TextField
                 required
                 name="firstName"
@@ -148,6 +167,7 @@ const VetSignUp = () => {
                 name="email"
                 label="Email"
                 variant="outlined"
+                type='email'
                 onChange={handleChange}
               />
             </Box>
@@ -181,7 +201,7 @@ const VetSignUp = () => {
                 }}
               />
             </Box>
-            <Box
+            {/* <Box
               component="form"
               sx={{
                 "& .MuiTextField-root": { m: 1, width: "52ch" },
@@ -210,7 +230,7 @@ const VetSignUp = () => {
                   ),
                 }}
               />
-            </Box>
+            </Box> */}
           </div>
           <Box
             sx={{
